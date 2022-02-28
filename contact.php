e52ebe08c789f71b5589c2673ee2c00ed07a13dd
Timothy974 <78727133+Timothy974@users.noreply.github.com>
2022-02-28 17:32:52 +0400

    add good link for video

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+
+if(isset($_POST['message'])) {
+
+   $retour = mail("timothy.gomis@gmail.com", "Message de mon site", $_POST['message'], "Reply-to:" . $_POST['email']);
+   if ($retour) {
+      echo "<p>L'e-mail à bien été envoyé";
+   }
+}
