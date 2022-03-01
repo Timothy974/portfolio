c1c788788c9497006f387a313626ec2db503fcc7
Timothy974 <78727133+Timothy974@users.noreply.github.com>
2022-03-01 14:29:39 +0400

    responsive ok

--- contact.php
+++ contact.php
@@ -4,6 +4,6 @@ if(isset($_POST['message'])) {
 
    $retour = mail("timothy.gomis@gmail.com", "Message de mon site", $_POST['message'], "Reply-to:" . $_POST['email']);
    if ($retour) {
-      echo "<p>L'e-mail à bien été envoyé";
+      echo "<p>L'e-mail à bien été envoyé</p>";
    }
 }
