44017a9f2b658d7f671ef0df78df7b1bd39a4bc4
Timothy974 <78727133+Timothy974@users.noreply.github.com>
2022-03-16 15:28:18 +0400

    sending mail via mailjet API

--- contact.php
+++ contact.php
@@ -1,9 +1,49 @@
 <?php
+  require 'vendor/autoload.php';
+  use \Mailjet\Resources;
 
-if(isset($_POST['message'])) {
+  $mj = new \Mailjet\Client('14cd026903525eceeb9d0d46d44fc522','fdd8b56fa159abb6d3126b4f8e30bc18',true,['version' => 'v3.1']);
 
-   $retour = mail("timothy.gomis@gmail.com", "Message de mon site", $_POST['message'], "Reply-to:" . $_POST['email']);
-   if ($retour) {
-      echo "<p>L'e-mail à bien été envoyé</p>";
-   }
-}
+  if(!empty($_POST['lastname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+     $lastname = htmlspecialchars($_POST['lastname']);
+     $firstname = htmlspecialchars($_POST['firstname']);
+     $email = htmlspecialchars($_POST['email']);
+     $message = htmlspecialchars($_POST['message']);
+
+     if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        $body = [
+          'Messages' => [
+            [
+              'From' => [
+                'Email' => "timothy.dev974@gmail.com",
+                'Name' => "tim"
+              ],
+              'To' => [
+                [
+                  'Email' => "timothy.dev974@gmail.com",
+                  'Name' => "tim"
+                ]
+              ],
+              'Subject' => "contact portfolio",
+              //'TextPart' => "My first Mailjet email",
+              'HTMLPart' => "$email, $message",
+              'CustomID' => "AppGettingStartedTest"
+            ]
+          ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+
+        echo "Email envoyé avec succès !";
+        header('refresh:3; url= https://timothy-gomis.go.yj.fr');
+
+     } else {
+        echo "email non valide";
+        header('refresh:3; url= https://timothy-gomis.go.yj.fr');
+     }
+  }
+?>
+
+
+
